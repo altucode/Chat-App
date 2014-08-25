7333282225f9821ee037894e81f129e450f7f479
Andrew Tu <altu2014@gmail.com>
2014-08-25 18:05:45 -0400

    Add user list.

--- public/javascripts/chat_ui.js
+++ public/javascripts/chat_ui.js
@@ -11,8 +11,16 @@ var displayMessage = function (message) {
   $(".chat-log").append($("<li></li>").text(message.nickname + ": " + message.text));
 };
 
+var renderRoomUsers = function (data) {
+  var list = $("<ul></ul>");
+  data.users.forEach(function (user) {
+    list.append($("<li></li>").text(user));
+  });
+  $(".user-list").html(list);
+};
 
 chat.socket.on("message", displayMessage);
+chat.socket.on("roomChangeEvent", renderRoomUsers);
 
 $(document).ready(function () {
   $("form").on("submit", sendMessage);
